424c6f13a06a5260afecab9c55f530e2007f6d61
figaric <sfigaric@gmail.com>
2021-04-24 23:57:47 +0300

    Add BasePage class
    A class that each page should inherit, to have its own viewmodel

---
+++ Source/Sorter.Desktop.Windows/Views/Pages/BasePage.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Sorter.Desktop.Windows
+{
+    public abstract class BasePage<TViewModel> : Page
+        where TViewModel : BaseViewModel, new()
+    {
+        public static TViewModel ViewModel { get; private set; }
+
+        public BasePage()
+        {
+            ViewModel = new TViewModel();
+        }
+    }
+}
