7a3ffad9f967f8d8e2adfa194ca8d89912f7a9d4
figaric <sfigaric@gmail.com>
2021-04-24 23:58:54 +0300

    Add a viewmodel for the MenuWindow

---
+++ Source/Sorter.Desktop.Windows/ViewModels/MenuWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Drawing;
+
+namespace Sorter.Desktop.Windows
+{
+    public class MenuWindowViewModel : BaseViewModel
+    {
+        private readonly MenuWindow _menuWindow;
+
+        #region Inner Padding Thickness
+
+        private readonly int _innerPaddingSize = 10;
+
+        public Thickness InnerPaddingSizeThickness => new Thickness(_menuWindow.WindowState == WindowState.Maximized ? 0 : _innerPaddingSize);
+
+        #endregion
+
+        #region Title Bar Thickness
+
+        private readonly int _titleBarThickness = 30;
+
+        public int CaptionHeight => _titleBarThickness + _innerPaddingSize;
+
+        public string TitleBarThickness => _titleBarThickness + "px";
+
+        #endregion
+
+        public MenuWindowViewModel(MenuWindow menuWindow)
+        {
+            _menuWindow = menuWindow;
+        }
+    }
+}
