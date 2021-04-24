aeaa53503e62b9236dc80ea4a8ab1ee07bec7931
figaric <sfigaric@gmail.com>
2021-04-24 23:56:41 +0300

    Add menu window

---
+++ Source/Sorter.Desktop.Windows/Views/Windows/MenuWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Sorter.Desktop.Windows
+{
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new MenuWindowViewModel(this);
+
+            PageFrame.Content = new StartUpPage();
+        }
+    }
+}
