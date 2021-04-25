cfc93177c45d53aa6238bd8e2600aa84761efba1
figaric <sfigaric@gmail.com>
2021-04-25 13:24:48 +0300

    Add RelayCommand class

---
+++ Source/Sorter.Core/Commands/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Sorter.Core
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _action;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action action)
+        {
+            _action = action ?? throw new ArgumentNullException("Command action cannot be null.");
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            _action.Invoke();
+        }
+    }
+}
