70e3f78e877c7512a948aa5a59f19d82ecedfcc6
figaric <sfigaric@gmail.com>
2021-04-25 13:25:41 +0300

    Add ExpressionHelpers class
    A static class that provides convenient usage of expressions

---
+++ Source/Sorter.Core/ExpressionHelpers/ExpressionHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Sorter.Core
+{
+    public static class ExpressionHelpers
+    {
+        public static T GetValue<T>(this Expression<Func<T>> expression) => expression.Compile().Invoke();
+
+        public static void SetValue<T>(this Expression<Func<T>> lambda, T value)
+        {
+            var expression = lambda.Body as MemberExpression;
+
+            var propertyInfo = (PropertyInfo)expression.Member;
+            var target = Expression.Lambda(expression.Expression).Compile().DynamicInvoke();
+
+            propertyInfo.SetValue(target, value);
+        }
+    }
+}
